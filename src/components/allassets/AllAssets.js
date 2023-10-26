372bab272243fc1fe3bb9748bf626b1c302556d8
Nicholas Williams <nicholaswilliams910@gmail.com>
2023-10-26 13:16:58 -0500

    displaying docks and haulers on main page and getter function for ship view with associated hauler

--- src/components/allassets/AllAssets.js
+++ src/components/allassets/AllAssets.js
@@ -34,12 +34,30 @@ export const AllAssetsList = () => {
 
   return (
     <div>
-      <h2>Ships</h2>
-      <article>
-        {allShips.map((shipObject) => {
-          return <div key={shipObject.id}>{shipObject.name}</div>;
-        })}
-      </article>
+      <div>
+        <h2>Ships</h2>
+        <article>
+          {allShips.map((shipObject) => {
+            return <div key={shipObject.id}>{shipObject.name}</div>;
+          })}
+        </article>
+      </div>
+      <div>
+        <h2>Docks</h2>
+        <article>
+          {allDocks.map((dockObject) => {
+            return <div key={dockObject.id}>{dockObject.location}</div>;
+          })}
+        </article>
+      </div>
+      <div>
+        <h2>Haulers</h2>
+        <article>
+          {allHaulers.map((haulerObject) => {
+            return <div key={haulerObject.id}>{haulerObject.name}</div>;
+          })}
+        </article>
+      </div>
     </div>
   );
 };
