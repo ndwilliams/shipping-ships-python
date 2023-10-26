bd843f7242290bdb6cd4dbd74a2192b604683e46
Nicholas Williams <nicholaswilliams910@gmail.com>
2023-10-26 13:07:41 -0500

    Fetch calls for ships, docks, and haulers. Displaying haulers and navbar on main page

--- src/components/allassets/AllAssets.js
+++ src/components/allassets/AllAssets.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import { getAllShips } from "../../services/shipsService";
+import { getAllDocks } from "../../services/docksService";
+import { getAllHaulers } from "../../services/haulersService";
 
 export const AllAssetsList = () => {
   const [allShips, setAllShips] = useState([]);
+  const [allDocks, setAllDocks] = useState([]);
+  const [allHaulers, setAllHaulers] = useState([]);
 
   const getAndSetShips = () => {
     getAllShips().then((shipsArray) => {
@@ -10,7 +14,32 @@ export const AllAssetsList = () => {
     });
   };
 
+  const getAndSetDocks = () => {
+    getAllDocks().then((docksArray) => {
+      setAllDocks(docksArray);
+    });
+  };
+
+  const getAndSetHaulers = () => {
+    getAllHaulers().then((haulersArray) => {
+      setAllHaulers(haulersArray);
+    });
+  };
+
   useEffect(() => {
     getAndSetShips();
+    getAndSetDocks();
+    getAndSetHaulers();
   }, []);
+
+  return (
+    <div>
+      <h2>Ships</h2>
+      <article>
+        {allShips.map((shipObject) => {
+          return <div key={shipObject.id}>{shipObject.name}</div>;
+        })}
+      </article>
+    </div>
+  );
 };
